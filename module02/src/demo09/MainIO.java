77c4b016295f747ffa147d44599821ec1a2c64a6
leofe <leofe200@163.com>
2020-04-12 16:28:22 +0800

    IO流

--- module02/src/demo09/MainIO.java
+++ module02/src/demo09/MainIO.java
@@ -1,5 +1,9 @@
 package demo09;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * IO 流
  *
@@ -39,7 +43,48 @@ package demo09;
  *
  */
 public class MainIO {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        copyFile();
+    }
 
+    // 复制文件
+    // 步骤：
+    // 1. 创建一个字节输入流对象，构造方法中传入要读取的数据源
+    // 2. 创建一个字节输出流对象，构造方法中传入要写入的目的地
+    // 3. 使用字节输入流中的 read 方法读取文件
+    // 4. 使用字节输出流中的 write 方法把读取到的字节写入到目的地文件
+    // 5. 释放资源
+    public static void copyFile() throws IOException {
+        long s = System.currentTimeMillis();
+
+        // 创建字节输入流对象
+        FileInputStream fis = new FileInputStream("module02\\src\\demo09\\java.png");
+        // 创建字节输出流对象
+        FileOutputStream fos = new FileOutputStream("module02\\src\\demo09\\java_copy.png");
+
+        // 读取一个字节写入一个字节
+//        int len = 0;
+//        // 读取一个字节
+//        while ((len = fis.read()) != -1 ) {
+//            // 写入一个字节
+//            fos.write(len);
+//        }
+
+        // 一次读写一个字节效率低，创建一个缓冲区，一次读写多个字节
+        byte[] bytes = new byte[1024];
+        // 每次读取的有效字节个数
+        int len = 0;
+        // 循环读取
+        while ((len = fis.read(bytes)) != -1) {
+            // 从字节数组索引 0 开始，写入有效字节个数
+            fos.write(bytes, 0, len);
+        }
+
+        // 释放资源(先关闭写，在关闭读)
+        fos.close();
+        fis.close();
+
+        long e = System.currentTimeMillis();
+        System.out.println("复制文件共耗时：" + (e - s) + "毫秒");
     }
 }
