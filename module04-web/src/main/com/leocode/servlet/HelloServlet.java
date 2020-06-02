688af4188a26797be6869d7beee02da56e451b51
liucong <leo-fe@foxmail.com>
2020-06-02 17:51:09 +0800

    Servlet

---
+++ module04-web/src/main/com/leocode/servlet/HelloServlet.java
@@ -0,0 +1,10 @@
+package main.com.leocode.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+
+
+}
